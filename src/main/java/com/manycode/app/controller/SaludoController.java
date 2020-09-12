06ff3d985bdd944f008633628faa0f14703d30db
ManONES <manuel.romero@siessoftware.net>
2020-09-12 13:49:11 -0500

    Our F C

--- src/main/java/com/manycode/app/controller/SaludoController.java
+++ src/main/java/com/manycode/app/controller/SaludoController.java
@@ -17,58 +17,26 @@ public class SaludoController {
     
     @Autowired
     WebClient.Builder webClientBuilder;    
-
-//    @RequestMapping("/Pase")
-//    public String inicio() {
-//        UserRating userRating = restTemplate.getForObject("http://DemoMicroservicios07/ratingsdata/user/MANY", UserRating.class);
-//        return (userRating.getUserId());        
-//    }
-
-
-    @RequestMapping("/Pase")
-    public String inicio() {
-//    	UserRating userRating = webClientBuilder.build().get().uri("http://localhost:8080/user/MANY")
-//    	UserRating userRating = webClientBuilder.build().get().uri("http://aplimicroservcios12.demomicroservcios12.svc.cluster.local/user/MANYONE")
-    	UserRating userRating = webClientBuilder.build().get().uri("aplimicroservcios12/user/MANYONE") 
-//    	    	UserRating userRating = webClientBuilder.build().get().uri("http://172.30.52.168:8080/user/MANY")
-    			.retrieve().bodyToMono(UserRating.class).block();
-    	return (userRating.getUserId());    	
-    }
     
-    
-    @RequestMapping("/Pase2")
-    public String iniciodos() {
-//    	UserRating userRating = webClientBuilder.build().get().uri("http://localhost:8080/user2/MANY")
-    	UserRating userRating = webClientBuilder.build().get().uri("aplimicroservcios12.5y6.svc.cluster.local/user/MANYTWO")
-//    	    	UserRating userRating = webClientBuilder.build().get().uri("http://172.30.52.168:8080/user/MANY")
+    @RequestMapping("/Pase1")
+    public String iniciouno() {
+    	//UserRating userRating = webClientBuilder.build().get().uri("http://demo-microservicios-7-git-hola.apps.us-west-1.starter.openshift-online.com/user/MANYTHREE")
+    	UserRating userRating = webClientBuilder.build().get().uri("http://app0601-proy06099.192.1.1.105.nip.io/user/MANYTHREE")
     			.retrieve().bodyToMono(UserRating.class).block();          
     	return (userRating.getUserId());    	
     }
     
-    
-    
-    @RequestMapping("/Pase3")
+    @RequestMapping("/PaseTest")
     public String iniciotres() {
-//    	UserRating userRating = webClientBuilder.build().get().uri("http://localhost:8080/user2/MANY")
     	UserRating userRating = webClientBuilder.build().get().uri("app6/user/MANYTHREE")
-//    	    	UserRating userRating = webClientBuilder.build().get().uri("http://172.30.52.168:8080/user/MANY")
     			.retrieve().bodyToMono(UserRating.class).block();          
     	return (userRating.getUserId());    	
     }
     
-    
-    @RequestMapping("/Pase4")
-    public String iniciocuatro() {
-//    	UserRating userRating = webClientBuilder.build().get().uri("http://localhost:8080/user2/MANY")
-    	UserRating userRating = webClientBuilder.build().get().uri("http://demo-microservicios-7-git-hola.apps.us-west-1.starter.openshift-online.com/user/MANYTHREE")
-//    	    	UserRating userRating = webClientBuilder.build().get().uri("http://172.30.52.168:8080/user/MANY")
-    			.retrieve().bodyToMono(UserRating.class).block();          
-    	return (userRating.getUserId());    	
-    }        
     
 	@GetMapping("/TestOrigen")
 	public String welcome() {
-		return "Hola al mundo de Many  con  microservciios 04150531";
+		return "Hola al mundo de Many  con  microservciios 09122020";
 	}	
 	
 }
